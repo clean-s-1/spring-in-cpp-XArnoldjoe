f8f7b0b2dc266b1f894121eaa8bf77ff16983f97
XArnoldjoe <104979869+XArnoldjoe@users.noreply.github.com>
2022-05-05 16:11:48 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,11 @@
 #include "stats.h"
+#include<bits/stdc++.h>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& ref ) {
     //Implement statistics here
+    Stats s;
+     s.average = (std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size());
+     s.max = *max_element(vec.begin(), vec.end());
+     s.min = *min_element(vec.begin(), vec.end());
+    return s;
 }
