5838135557729da78bbae5ae7f8c89c850631df0
XArnoldjoe <104979869+XArnoldjoe@users.noreply.github.com>
2022-05-05 16:31:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& vec ) {
+     Stats s;
+    if(vec.empty())
+        return s;
     //Implement statistics here
-    Stats s;
+   
      s.average = (std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size());
      s.max = *max_element(vec.begin(), vec.end());
      s.min = *min_element(vec.begin(), vec.end());
